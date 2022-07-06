195056d7fc5d8fea0786a87e433fdb8e126ab667
sujoygiri <girisujoy630@gmail.com>
2022-07-06 21:29:56 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
+
 function App() {
+
+  const [quote, setQuote] = useState(null);
+  const random_number = Math.floor((Math.random() * 100) + 1);
+  const [randomNumber, setRandomNumber] = useState(random_number);
+
+  const fetchQuote = async () => {
+    const url = 'https://type.fit/api/quotes'
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    console.log(response);
+    if (response.status === 200) {
+      const data = await response.json();
+      setQuote(data);
+    }else{
+      console.log('Something went wrong!');
+    }
+  }
+
+  const generateRandomNumber = () => {
+    let range = quote.length;
+    let number = Math.floor((Math.random() * range) + 1);
+    setRandomNumber(number);
+  }
+
+  useEffect(() => {
+    fetchQuote();
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="quote-box">
+          <div className="quote">
+            <div className="quote-body">
+              <q className="quote-text">
+                {quote && quote[randomNumber].text}
+              </q>
+            </div>
+                {quote && quote[randomNumber].author}
+          </div>
+          <div className="quote-bottom">
+            <button className="new-quote-btn" onClick={generateRandomNumber}>Next Quote</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
